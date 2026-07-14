4dfc9581c317536e1fc34fef304b48d39f03bae8
prathameshx-coder <prathameshpatil7214@gmail.com>
2026-07-14 15:51:28 +0530

    Add files via upload

---
+++ hello.cpp.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2,num3;
+cout<<"enter first number:";
+cin>>num1;
+cout<<"enter second number:";
+cin>>num2;
+num3= num1+num2;
+cout<<"sum="<<sum<<endl;
+return0;
+}
