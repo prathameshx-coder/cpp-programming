236d0c03c8e10975503ff1402e49cc195b9616b5
prathameshx-coder <prathameshpatil7214@gmail.com>
2026-07-28 08:55:09 +0000

    success

---
+++ leapyear.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"enter a year:";
+    cin>>year;
+    if(year%4==0){
+        cout<<"leap year";
+    }
+    else
+    {
+        cout<<"not a leap year";
+    }
+}
