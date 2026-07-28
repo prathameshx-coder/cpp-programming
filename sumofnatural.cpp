d2818efba50d3f877e3ec0eb081774ca19a125d5
prathameshx-coder <prathameshpatil7214@gmail.com>
2026-07-28 09:39:11 +0000

    success

---
+++ sumofnatural.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n; i++)
+    {
+    sum+=i;
+    }
+    cout<<"sum of first" <<n<< "number is="<<sum;
+}
