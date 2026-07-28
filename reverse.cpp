0c61eba1b3e2544455a61333cc39b60bc7bc0c9d
prathameshx-coder <prathameshpatil7214@gmail.com>
2026-07-28 09:54:22 +0000

    success

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, reverse=0, remainder;
+    cout<<"enter an integer:";
+    cin>>num;
+    while(num!=0)
+    {
+        remainder=num% 10;
+        reverse= reverse*10+ remainder;
+        num=num/10;
+    }
+    cout<<"reversed number="<< reverse<< endl;
+}
